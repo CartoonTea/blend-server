7923f67420cb52abdb837a36bbf67d1c6ff2ce6d
Connor Bode <connor.bode@gmail.com>
2015-01-11 16:42:09 -0500

    CHANGEZZZZZ

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ gulp.task('clean', [], function (cb) {
   del(['build'], cb);
 });
 
-gulp.task('default', ['clean'], function (cb) {
+gulp.task('build', ['clean'], function (cb) {
   return gulp.src('app/**/*')
     .pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', ['build'], function (cb) {
 });
