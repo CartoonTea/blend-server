c6fdb9bea37656e71b9c96e41214b7b11fba2bbc
Connor Bode <connor.bode@gmail.com>
2015-01-11 16:25:57 -0500

    added a method that cleans the build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
+var del = require('del');
 
-gulp.task('default', [], function (cb) {
-  gulp.src('app/**/*')
+gulp.task('clean', [], function (cb) {
+  del(['build'], cb);
+});
+
+gulp.task('default', ['clean'], function (cb) {
+  return gulp.src('app/**/*')
     .pipe(gulp.dest('build/'));
 });
