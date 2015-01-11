abc48a861b4306818bb1bd5cf5199f7297e91b3d
Greg Houle <houle.greg@gmail.com>
2015-01-11 17:52:42 -0500

    minor mod
    added .env to gitignore
    added the post to callback function
    added github api to packages

--- app/main.js
+++ app/main.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
+app.post('/callback', function(req, res) {
+
+});
+
 app.listen(3000);
 
 console.log('listening on port 3000');
