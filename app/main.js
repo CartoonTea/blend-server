fa3b300e9be21382b79c40489eed3d7db25a1652
Greg Houle <houle.greg@gmail.com>
2015-01-11 19:08:22 -0500

    post code. receive access token fun

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
+var http = require('http');
 var express = require('express');
 var app = express();
-var env = 
 
 app.use('/static', express.static(__dirname + '/static'));
 
@@ -9,7 +9,36 @@ app.get('/', function (req, res) {
 });
 
 app.get('/callback', function(req, res) {
-    req.query.code
+    var code = req.query.code;
+    var body = {
+        client_id: env.client_id,
+        client_secret: env.client_secret,
+        code: code
+    };
+
+    var opt = {
+        host: 'https://github.com/login/oauth/access_token',
+        path: '/',
+        port: '3000',
+        method: 'POST'
+    };
+
+    var cb = function(response) {
+        var data = '';
+
+        response.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        response.on('end', function () {
+            var username = '';
+            var access_token = '';
+            console.log(data);
+        });
+    }
+    var req_ = http.request(opt, cb);
+    req_.write(JSON.stringify(body));
+    req_.end();
     
 
 });
